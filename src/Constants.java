e70eb5c2062c18b953a6a8492bd66be77af6584a
ifurss <zabelincs@gmail.com>
2023-08-20 12:37:34 +0200

    20-08-2023: Constants for the project

---
+++ src/Constants.java
@@ -0,0 +1,16 @@
+public class Constants {
+
+   static String lulw = "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,”:-!? ";
+    private static final char[] KEKW = lulw.toCharArray();
+    private static final int KEY = 3;
+
+    public static char[] getArray() {
+        return KEKW;
+    }
+    public static int getKEY() {
+        return KEY;
+    }
+
+
+
+}
